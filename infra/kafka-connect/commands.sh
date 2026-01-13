cf310c5baf5fd48452b88b1d491768631c5e3c0a
Aleksandr Okunev <okunev@axlebolt.com>
2026-01-13 14:52:44 +0500

    players debezium

--- infra/kafka-connect/commands.sh
+++ infra/kafka-connect/commands.sh
@@ -4,6 +4,8 @@ curl -X POST --data-binary "@./connectors/source/players_source.json" -H "Conten
 
 curl -X POST --data-binary "@./connectors/source/payment_outbox_source.json" -H "Content-Type: application/json" http://localhost:8083/connectors | jq
 
+curl -X POST --data-binary "@./connectors/source/equipment_source.json" -H "Content-Type: application/json" http://localhost:8083/connectors | jq
+
 curl -X POST --data-binary "@./connectors/source/product_trade_outbox_source.json" -H "Content-Type: application/json" http://localhost:8083/connectors | jq
 
 curl -X POST --data-binary "@./connectors/sink/keycloak_players_sink.json" -H "Content-Type: application/json" http://localhost:8083/connectors | jq
